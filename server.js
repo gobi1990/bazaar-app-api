dd70c98cd3e2d07f601dc999a737420fc0785fa3
Gobishankar Annalingam <gobishankar.a@armiasystems.com>
2024-03-30 00:49:10 -0400

    db access issue fixes

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
 
 app.get('/api/data', (req, res) => {
     const data = [
